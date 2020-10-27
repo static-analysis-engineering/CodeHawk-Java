4302a5b2a58459d7d40e400189cb93f8d1fb35e2
Andrew McGraw <mcgraw4@illinois.edu>
2020-10-26 17:05:40 -0700

    Add missing file

---
+++ chj/util/analysisutil.py
@@ -0,0 +1,51 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Java Analyzer
+# Author: Andrew McGraw
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2020 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import os
+import subprocess
+
+import chj.util.fileutil as UF
+import chj.cmdline.AnalysisManager as AM
+
+def analyze_taint_propagation(appname, origin):
+    try:
+        UF.check_analyzer()
+        (path,jars) = UF.get_engagement_app_jars(appname)
+        UF.check_analysisdir(path)
+    except UF.CHJError as e:
+        print(str(e.wrap()))
+        return
+
+    pkg_excludes = UF.get_engagement_app_excludes(appname)
+    dependencies = UF.get_engagement_app_dependencies(appname)
+
+    am = AM.AnalysisManager(path,jars,dependencies=dependencies,excludes=pkg_excludes)
+
+    try:
+        am.create_taint_trail(origin,silent=True)
+    except UF.CHJError as e:
+        print(str(e.wrap()))
